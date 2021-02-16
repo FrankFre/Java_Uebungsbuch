1612a7021dff880708461a5fbc79562ad8594753
Frank Freisberg <f.freisberg@telekom.de>
2021-02-16 14:20:01 +0100

    Import-Anweisung

---
+++ src/kapitel1/paket1/KlassenTest.java
@@ -0,0 +1,12 @@
+package kapitel1.paket1;
+
+public class KlassenTest {
+
+    public static void main(String[] args) {
+        paket2.Klasse kls = new paket2.Klasse() {
+
+            //wird die Import-Anweisung genutzt, kann auf den Klassennamen direkt zugegriffen werden
+        };
+    }
+
+}
---
+++ src/kapitel1/paket1/paket2/Klasse.java
@@ -0,0 +1,8 @@
+package paket2;
+
+public class Klasse {
+    public Klasse()  {
+        System.out.println("Definition einer Klasse im Verzeichnis paket2");
+
+    }
+}
