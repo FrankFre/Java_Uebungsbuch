625fbc59e6573638dd140810106e89994177ee8e
Frank Freisberg <f.freisberg@telekom.de>
2021-02-18 13:48:09 +0100

    final - fast kein Fehler

--- src/WrapperClassenMitAutoBoxingTest.java
+++ src/WrapperClassenMitAutoBoxingTest.java
@@ -4,19 +4,29 @@ public class WrapperClassenMitAutoBoxingTest {
 
         //Objekt der Klasse erzeugen
         WrapperKlassemitAutoBoxing  wrapper = new WrapperKlassemitAutoBoxing
-                ((byte)1,2, '3', (short)4, (long)5,(float)1.0,2.0,true);
+                ((byte)1,2, '3', (short)4,(long)5,(float)1.0,2.0,true);
         System.out.println("Basistypwerte der Wrapper-Objekte");
 
         //und ihre Methoden aufrufen
         wrapper.objectTonumber();
 
+        System.out.println("Auto(un)boxing in Methoden-aufrufen und " + "-rueckgaben: ");
+        System.out.println((wrapper.konvert(true)));
+//        System.out.println(wrapper.einBoolean);
 
+        System.out.println("Typumwandungen in einem Ausdruck:");
+        wrapper.rechnen(1, 2.0);
 
+        System.out.println("Unboxing in Kontrollanweisungen");
+        wrapper.vergleichen(1, 2);
 
+        //Anstelle des mit deprecated gekennzeichneten Konstruktors kann die Klassenmethode Integer.valueOf() aufgerufen werden
+        //in dieser Zeile ist ein Bug
+        wrapper.vergleichen(Integer.valueOf(1), Integer.valueOf(1));
 
+        Integer zahl1 = Integer.valueOf(1);
+        Integer zahl2 = zahl1;
+        wrapper.vergleichen(zahl1, zahl2);
 
     }
-
-
-
 }
--- src/WrapperKlassemitAutoBoxing.java
+++ src/WrapperKlassemitAutoBoxing.java
@@ -1,7 +1,7 @@
 public class WrapperKlassemitAutoBoxing {
 
     //Globale Referenzen von Typ der Hüllenklassen
-    private Boolean einBoolean;
+    Boolean einBoolean;
     private Character einCharacter;
 
     private Byte einByte;
@@ -12,11 +12,12 @@ public class WrapperKlassemitAutoBoxing {
     private Double einDouble;
 
     //Konstruktordefinitionen
-    WrapperKlassemitAutoBoxing(boolean bo, char c, byte by, short s, int i, long l, float f, double d) {
+    WrapperKlassemitAutoBoxing(byte by, int i, char c, short s, long l, float f, double d, boolean bo) {
         //Instanzen der Hüllklassen erzeugen
+
+        einByte = by;
         einBoolean = bo;
         einCharacter = c;
-        einByte = by;
         einShort = s;
         einInteger = i;
         einFloat = f;
@@ -25,7 +26,7 @@ public class WrapperKlassemitAutoBoxing {
     }
 
     // Lieferung des Basistyp-Werts eines Wrapper-Objekts über Unboxing
-    public void ObjectTonumber() {
+    public void objectTonumber() {
         System.out.println(" " + einByte);
         System.out.println(" " + einInteger);
         System.out.println(" " + einLong);
@@ -53,47 +54,44 @@ public class WrapperKlassemitAutoBoxing {
         Double ergebnis = zahlFloat * zahlDouble + zahlFloat / zahlDouble;
         System.out.println("Wert von Inkrementieren: " + ergebnis);
 
-        //Das Wrapper-Objekt ergebnis wird entkapselt, der gewonnene Wert inkrementiert und wieder eingehüllt
+        //Das Wrapper-Objekt Ergebnis wird entkapselt, der gewonnene Wert inkrementiert und wieder eingehüllt
         ergebnis++;
         System.out.println("Wert nach Inkrementieren: " + ergebnis);
         System.out.print(
                 "Ausdruck von Typ Integer in einer for-Schleife "
         );
-        for (Integer i = 0; i < 2; i++) {
+        for (Integer i = 0; i <= 2; i++) {
             System.out.print(i + " ");
             System.out.println();
         }
+    }
 
+    //Unboxing mit Kontrollanweisungen
+    public void vergleichen(Integer zahl1Integer, Integer zahl2Integer) {
 
-        //Unboxing mit Kontrollanweisungen
-        public void vergleichen(Integer zahl1Integer, Integer zahl2Integer){
+        //Unboxing von Wrapper-Objekten
+        int zahl1 = zahl1Integer;
+        int zahl2 = zahl2Integer;
 
-            //Unboxing von Wrapper-Objekten
-            int zahl1 = zahl1Integer;
-            int zahl2 = zahl2Integer;
+        // Vergleichen von Referenzen
+        if (zahl1Integer == zahl2Integer) {
+            System.out.println("Gleiche Referenzen");
 
-            // Vergleichen von Referenzen
-            if (zahl1Integer == zahl2Integer) {
-                System.out.println("Gleiche Referenzen");
-
-                //Vergleich von primitiven Datentypen
-                if (zahl1 == zahl2)
-                    System.out.println(" und gleiche Werte");
-                else
-                    System.out.println(" und verschiedene Werte");
-            } else {
-                System.out.println("Verschiedene Referenzen");
-                if (zahl1 == zahl2)
-                    System.out.println(" und gleiche Werte");
-                else
-                    System.out.println(" und verschiedene Werte");
-
-            }
+            //Vergleich von primitiven Datentypen
+            if (zahl1 == zahl2)
+                System.out.println(" und gleiche Werte");
+            else
+                System.out.println(" und verschiedene Werte");
+        } else {
+            System.out.println("Verschiedene Referenzen");
+            if (zahl1 == zahl2)
+                System.out.println(" und gleiche Werte");
+            else
+                System.out.println(" und verschiedene Werte");
 
         }
 
-    }
-}
+    }}
 
 
 
